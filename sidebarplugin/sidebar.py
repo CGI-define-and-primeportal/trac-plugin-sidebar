c883ac321b7c24400895cf7382134225b2640cee
Danny Milsom <danny.milsom@cgi.com>
2013-05-14 21:23:01 +0000

    Creating another branch for my external users requirement #3129 #3169
    git-svn-id: file:///mnt/ramdisk/d4-svn/branches/features/creatingexternalaccounts@10197 484df14f-75d2-4b3f-8ba0-490e05069a01

---
+++ sidebarplugin/sidebar.py
@@ -0,0 +1,43 @@
+from trac.core import Component, implements, TracError
+from trac.web.chrome import add_stylesheet, ITemplateProvider
+from trac.web.api import ITemplateStreamFilter
+from genshi.builder import tag
+from genshi.filters.transform import Transformer
+from api import ISidebarBoxProvider
+from trac.core import ExtensionPoint
+from pkg_resources import resource_filename
+
+try:
+    from simplifiedpermissionsadminplugin import SimplifiedPermissions
+except ImportError, e:
+    SimplifiedPermissions = None
+
+class SidebarSystem(Component):
+    """The sidebar system will call all ISidebarBoxProvider's get_box() in turn
+        and append the result. The resulting tags will be placed in the 
+        beginning of the <div id=content> tag of all wiki pages."""
+    
+    implements(ITemplateStreamFilter, ITemplateProvider)
+    sidebar_boxes = ExtensionPoint(ISidebarBoxProvider)
+    # ITemplateProvider methods
+    def get_htdocs_dirs(self):
+        return [('sidebar', resource_filename(__name__, 'htdocs'))]
+
+    def get_templates_dirs(self):
+        return [resource_filename('sidebarplugin', 'templates')]
+
+    # ITemplateStreamFilter
+    def filter_stream(self, req, method, filename, stream, data):
+        sidebar_class = "sidebar span3"
+        if not filename == "wiki_view.html":
+            return stream
+        if not data['page'].name == "WikiStart":
+            return stream
+        #add_stylesheet(req, "sidebar/css/sidebar.css")
+        transformers = []
+        stream = stream | Transformer("//div[@id='wiki-page-inside']").attr("class", "span9").after(tag.div(class_=sidebar_class))
+        for box in self.sidebar_boxes:
+            box_detail = box.get_box(req)
+            if box_detail:
+                transformers.append(Transformer("//div[@class='" + sidebar_class + "']").prepend(box_detail))
+        return stream.filter(*transformers)
